76e930cd0b054f41efd356da8d93850461dfdeb4
Stefan Holmberg <stefan.holmberg@systementor.se>
2026-06-02 08:30:00 +0200

    p

--- API/fetchCart.php
+++ API/fetchCart.php
@@ -4,7 +4,7 @@ require_once(__DIR__.'/../Models/Database.php');
 require_once(__DIR__.'/../Models/Cart.php');
 require_once(__DIR__.'/../Models/CartItem.php');
 
-$productIdToAddToCart = $_GET['id'];
+
 
 $database = new Database();
 $cart = new Cart($database, session_id());
